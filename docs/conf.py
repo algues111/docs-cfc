ebf117d1b57ba93f976de2c4e2663d4ccbad948a
algues111 <a.leguennec909@proton.me>
2024-05-06 14:25:56 +0200

    changes

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,12 @@ release = '0.1'
 
 extensions = [
     'myst_parser',
+    'sphinx_tabs.tabs',
     
 ]
+
+sphinx_tabs_valid_builders = ['linkcheck']
+
 myst_enable_extensions = [
     "amsmath",
     "attrs_inline",
